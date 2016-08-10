7f67e196a52e6d4a17d50ff45fd32be8aa7b1e17
Brian Pattison <brian@brianpattison.com>
2016-08-10 16:34:11 -0500

    Update Codelation CLI formula

--- codelation-cli.rb
+++ codelation-cli.rb
@@ -1,8 +1,8 @@
 class CodelationCli < Formula
   desc "Command line tool for Codelation development tasks."
   homepage "https://github.com/codelation/codelation-cli"
-  url "https://github.com/codelation/codelation-cli/releases/download/v0.3.0/codelation-0.3.0.tar.gz"
-  sha256 "bbb7397aa6b77b9cb6b7e5079fbf47cc7d707163d8a111cedb99114e9beb5972"
+  url "https://github.com/codelation/codelation-cli/releases/download/v0.3.1/codelation-0.3.1.tar.gz"
+  sha256 "9c177ca48456c844f8d513376e719cdc368c64bec354aabf77885c2c387c597d"
 
   bottle :unneeded
 
