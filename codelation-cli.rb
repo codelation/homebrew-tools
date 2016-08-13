e528b7768bd065e20341b45282bf592449cfdda5
Brian Pattison <brian@brianpattison.com>
2016-08-13 10:56:13 -0500

    Add bdw-gc dependency to codelation cli

--- codelation-cli.rb
+++ codelation-cli.rb
@@ -1,10 +1,12 @@
 class CodelationCli < Formula
   desc "Command line tool for Codelation development tasks."
   homepage "https://github.com/codelation/codelation-cli"
-  url "https://github.com/codelation/codelation-cli/releases/download/v0.3.1/codelation-0.3.1.tar.gz"
-  sha256 "9c177ca48456c844f8d513376e719cdc368c64bec354aabf77885c2c387c597d"
+  url "https://github.com/codelation/codelation-cli/releases/download/v0.3.2/codelation-0.3.2.tar.gz"
+  sha256 "ab2c3f041e5ac70b3811222bac3e277c1fcb6b7b08833cba0d1483d1bd2d3ac9"
 
   bottle :unneeded
+
+  depends_on "bdw-gc"
 
   def install
     bin.install "codelation"
