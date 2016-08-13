ca170721b342d67133be97f7028af0353c3ce12c
Brian Pattison <brian@brianpattison.com>
2016-08-13 11:02:13 -0500

    Add version to codelation-cli

--- codelation-cli.rb
+++ codelation-cli.rb
@@ -3,6 +3,7 @@ class CodelationCli < Formula
   homepage "https://github.com/codelation/codelation-cli"
   url "https://github.com/codelation/codelation-cli/releases/download/v0.3.2/codelation-0.3.2.tar.gz"
   sha256 "ab2c3f041e5ac70b3811222bac3e277c1fcb6b7b08833cba0d1483d1bd2d3ac9"
+  version "0.3.2"
 
   bottle :unneeded
 
