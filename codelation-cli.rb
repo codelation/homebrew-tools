5c86c83f24fd0b931f11a3774bf14a36bba5278c
Brian Pattison <brian@brianpattison.com>
2016-08-15 11:09:14 -0500

    Update Codelation CLI to latest version

--- codelation-cli.rb
+++ codelation-cli.rb
@@ -1,9 +1,9 @@
 class CodelationCli < Formula
   desc "Command line tool for Codelation development tasks."
   homepage "https://github.com/codelation/codelation-cli"
-  url "https://github.com/codelation/codelation-cli/releases/download/v0.3.3/codelation-0.3.3.tar.gz"
-  sha256 "bbf647dcd3a3a2858b649357c2bfd190f1cf3f631c49833c20bf1dc64843d8d0"
-  version "0.3.3"
+  url "https://github.com/codelation/codelation-cli/releases/download/v0.3.4/codelation-0.3.4.tar.gz"
+  sha256 "6465db37f7151510e66b053c8412754920666dc582596f281ecb5412c7b99697"
+  version "0.3.4"
 
   bottle :unneeded
 
