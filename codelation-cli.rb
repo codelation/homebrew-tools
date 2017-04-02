d9eef74f6a38d04f51d48cff079be4fd133f9148
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-02 13:16:43 -0500

    Update codelation-cli.rb

--- codelation-cli.rb
+++ codelation-cli.rb
@@ -1,9 +1,9 @@
 class CodelationCli < Formula
   desc "Command line tool for Codelation development tasks."
   homepage "https://github.com/codelation/codelation-cli"
-  url "https://github.com/codelation/codelation-cli/releases/download/v2.0.0/codelation-2.0.0.tar.gz"
-  sha256 "12749ae7718e82e53436e417b9fc34b451be7d035733809369e963c2b276cad6"
-  version "2.0.0"
+  url "https://github.com/codelation/codelation-cli/releases/download/v2.0.1/codelation-2.0.1.tar.gz"
+  sha256 "e29674e96eacb531108b7eb0a20491b09c70b2bee3a54adb5195f9a2fc4e697a"
+  version "2.0.1"
 
   bottle :unneeded
 
